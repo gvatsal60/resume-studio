a206643e2afbb3510707e485b5fdab6a5a43be1e
Vatsal Gupta <40350810+gvatsal60@users.noreply.github.com>
2026-07-19 10:37:11 +0000

    refactor: extract duplicated selectors/scripts into shared constants

---
+++ frontend/constants.js
@@ -0,0 +1,4 @@
+const AUTOPREVIEW_CHECKBOX = "#autopreview";
+const THEME_SELECT = "#theme-select";
+const EDITOR_SELECT = "#editor";
+const BOLD_BUTTON = ".format-toolbar--global [data-action='bold']";
